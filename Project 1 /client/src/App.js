f7f00f59c480d877f27bebc71628495904c2f63b
estebansamayoa1 <61565155+estebansamayoa1@users.noreply.github.com>
2021-10-04 09:35:09 -0600

    App para correr

---
+++ Project 1 /client/src/App.js
@@ -0,0 +1,17 @@
+import React, {Fragment} from "react";
+import './App.css';
+
+
+//components
+import SignUp from "./components/SignUp";
+
+
+function App() {
+    return <Fragment>
+      <div className="container">
+        <SignUp/>
+      </div>
+    </Fragment>
+}
+
+export default App;
